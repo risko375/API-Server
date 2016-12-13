221797121cac7a530c17c69773756bcbfb5b4fa4
Richard James <richard.james@mygravity.co>
2016-12-13 16:40:05 +0000

    Completed project

---
+++ router.js
@@ -0,0 +1,20 @@
+const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// session: false prevents creation of cookie as we are using token
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
+
+module.exports = function(app){
+    
+    app.get('/', requireAuth, function(req, res){
+        res.send({ hi: 'there' });
+        
+    });
+    
+    app.post('/signin', requireSignin, Authentication.signin);
+    
+    app.post('/signup', Authentication.signup);
+    
+}
